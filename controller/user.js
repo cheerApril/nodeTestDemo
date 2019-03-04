e4a42cef9220c126228473de8f96df7e8cb5a039
zhangchaowei <314998478@qq.com>
2019-03-04 19:03:36 +0800

    简单的create 跟update接口上传

--- controller/user.js
+++ controller/user.js
@@ -11,14 +11,55 @@ module.exports = {
     },
     
     create: function (req, res, next) {
-        
+        let createData = {};
+        for(let [index, value] of  createData){
+            switch (index) {
+                case "name":
+                    createData[index] = value;
+                    break;
+                case "age":
+                    createData[index] = value;
+                    break;
+                default:
+                    break;
+            }
+        }
+        modelUser.create(createData).then((result) =>{
+            res.success(result);
+        }).catch((err) =>{
+            next(err);
+        })
     },
     
     update: function (req, res, next) {
-        
+        const id = req.params.id;
+        let updateData = {};
+        for(let [index, value] of  req.body){
+            switch (index) {
+                case "name":
+                    updateData[index] = value;
+                    break;
+                case "age":
+                    updateData[index] = value;
+                    break;
+                default:
+                    break;
+            }
+        }
+        modelUser.update(updateData,
+            {
+                where: {
+                    id: id
+                }
+            }
+        ).then((result) =>{
+            res.success(result);
+        }).catch((err) =>{
+            next(err);
+        })
     },
     
     delete: function (req, res, next) {
-        
+
     }
 };
---
+++ demo/test1.js
@@ -0,0 +1,4 @@
+const length = 10;
+for(let i = 0; i < 10; i++){
+    console.log(Math.floor(Math.random() * 10) + 0);
+}
