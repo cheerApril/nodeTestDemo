0a0c72dbeaf6a9de2102ac4ba09ff3bd12226899
zhangchaowei <314998478@qq.com>
2019-03-25 15:10:03 +0800

    增加multer的包, POST的请求方式增加

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const path = require(`path`);
+const multer = require(`multer`);
 const app = express();
-// 中间件使用 -- req.body能获取到body的数据的原因
+//中间件使用 req.body (HTTP 请求头headers content_type form-data 获取到文本或者文件的原因)
+//警告: 确保你总是处理了用户的文件上传。
+// 永远不要将 multer 作为全局中间件使用，因为恶意用户可以上传文件到一个你没有预料到的路由，应该只在你需要处理上传文件的路由上使用。
+//var storage = multer.memoryStorage() // 内存存储引擎将文件存储在内存中的 Buffer 对象，它没有任何选项。
+// var upload = multer({ storage: storage })
+//警告: 当你使用内存存储，上传非常大的文件，或者非常多的小文件，会导致你的应用程序内存溢出。
+
+
+const upload = multer();
+// 中间件使用 -- req.body能获取到body的数据的原因(HTTP 请求头headers content_type x-www-form-urlencoded)
 const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 //模板中间件设置(设置相关文件 + 模块)
@@ -38,6 +48,13 @@ const port = normalizePort(process.env.PORT || '3000');
 const indexRouter = require(`./routes/index.js`);
 const adminRouter = require(`./routes/admin.js`);
 const userRouter = require('./routes/users.js');
+//
+// //增加测试接口
+// app.use(`/test`, upload.single(`image`), function (req, res, next) {
+//     console.log(typeof req.body);
+//     res.success(req.file);
+// });
+
 app.use(`/`, indexRouter);
 app.use('/user', userRouter);
 app.use(`/admin`, adminRouter);
